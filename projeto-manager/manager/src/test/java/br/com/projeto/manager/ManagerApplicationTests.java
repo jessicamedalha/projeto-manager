9b1c0d0f8e5a7ed606843a9cad89cafa2aaeb917
jessicamedalha <jessicamedalhnha@gmail.com>
2023-04-11 12:54:42 -0300

    estrutura inicial

---
+++ projeto-manager/manager/src/test/java/br/com/projeto/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
